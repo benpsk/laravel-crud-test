e88903f48e3e84850fe203906e987c7ae9aec2de
Gunn09 <bengunnlove9@gmail.com>
2021-01-13 00:30:33 +0630

    Initial commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel 8 & MySQL CRUD Tutorial</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</head>
+<body>
+  <div class="container pt-4">
+    @yield('main')
+  </div>
+ 
+</body>
+</html>
